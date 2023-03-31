c1e5f4de2f4e106b3c545ed260d0cef442bc1952
Jeff Moore <jmoore@manifold.ai>
2023-03-31 08:38:15 -0600

    feat: define and install minimalist vimrc for remote use

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ cp zshrc ~/.zshrc
 cp bash_profile ~/.bash_profile
 cp bash_aliases ~/.bash_aliases
 cp gitconfig ~/.gitconfig
+cp vimrc ~/.vimrc
 shell=basename $SHELL
 if [ $shell = "bash" ]; then
     source ~/.bash_profile
