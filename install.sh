ea61c4adb80062a28ae76ceea1f0550a3a8480e5
Jeff Moore <jmoore@manifold.ai>
2023-03-31 08:54:18 -0600

    fix: install script

--- install.sh
+++ install.sh
@@ -5,11 +5,13 @@ cp bash_profile ~/.bash_profile
 cp bash_aliases ~/.bash_aliases
 cp gitconfig ~/.gitconfig
 cp vimrc ~/.vimrc
-shell=basename $SHELL
+shell=$(basename $SHELL)
 if [[ $shell == "bash" ]]; then
     source ~/.bash_profile
+    source ~/.bash_aliases
 elif [[ $shell == "zsh" ]]; then
     source ~/.zshrc
+    source ~/.bash_aliases
 else
     source ~/.bash_aliases
 fi
